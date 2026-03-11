4a917a8a0c9e32791a4ca81aab6dbc8cd9a5a893
Soulrom <mokriiromuch@gmail.com>
2026-03-11 09:20:05 +0200

    Add Dealer class

---
+++ src/dealer.py
@@ -0,0 +1,31 @@
+class Dealer:
+    def __init__(self):
+        self.hand = []
+        self.hidden_card = None
+    
+    def add_card(self, card):
+        self.hand.append(card)
+
+    def clear_hand(self):
+        self.hand = []
+    
+    def get_score(self):
+        score = 0
+        aces = 0
+
+        for card in self.hand:
+            score += card.get_numeric_value()
+            if card.value == 'A':
+                aces += 1
+        
+        while score > 21 and aces > 0:
+            score -= 10
+            aces -= 1
+        
+        return score
+
+    def reveal(self):
+        self.hand.append(self.hidden_card)
+
+    def should_hit(self):
+        return self.get_score() < 17
